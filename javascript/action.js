3cba9662c756097a0c09a95b02e57a15d2ef0f20
Nathan Leavitt <nateleavitt@gmail.com>
2019-10-24 16:45:50 -0700

    updating

--- javascript/action.js
+++ javascript/action.js
@@ -1,55 +1,42 @@
 function addListParent(listName) {
-    
-    let listIndex = myList.add(listName) - 1;
-    console.log(listIndex);
-    $(".listName").val("");
-    $(".lists").append("<li class='listItem' data-index='" + listIndex + "'>" + listName + "</li>");
 
+    $("#newTodoDiv").show();
+    myList.add(listName);
+    printPage();
 
-    // printPage();
-    
     console.log("This is working");
 }
 
 function addListChild(itemName, listNum) {
-    // switch (event.which) {
-    //     case 13: //enter key
-            $(".createListNames").val("");
-            myList.collection[listNum].add(itemName, "Name of the List Items");
-
-            // printPage();
-            // break;
-    // }
+    $(".createListNames").val("");
+    myList.collection[listNum].add(itemName, "Name of the List Items");
+    printPage();
 }
 
 function editList(listNum, itemnumber, name) {
-    switch (event.which) {
-        case 13:
-            let thename = myList.add(thename)
-            // thename = name;
-            printPage();
-            break;
-    }
+    let thename = myList.add(thename)
+    // thename = name;
+    printPage();
 }
 
 
 function printPage() {
     console.log("printPage function");
 
-    $(".listNames").html("");
+    $(".todoList").html("");
 
     for (let i = 0; i < myList.collection.length; i++) {
         let theli = myList.collection[i];
         let listmembers = "";
         for (let g = 0; g < theli.collection.length; g++) {
             let liName = theli.collection[g];
-            listmembers += `<div class="member" contenteditable="true" onkeydown="editList(${i}, ${g}, this.innerText)" >${liName.name}</div>`
+            listmembers += `<li class="member" contenteditable="true" onkeydown="editList(${i}, ${g}, this.innerText)" >${liName.name}</li>`
         }
-        $(".listNames").append(`                        
-                        <input type="text" class="listparent" onkeyup="addListChild(event, this.value, ${i})">
-                    `)
-                    
-                    /* this is code that may come in handy later on but im not positive.... 
+        // $(".todoList").append(`<input type="text" class="listparent" onkeyup="addListChild(event, this.value, ${i})">`);
+        $(".lists").append(`<li type="text" class="listparent" onkeyup="addListChild(event, this.value, ${i})">${theli.name}</li>`);
+        $(".todoList").append(listmembers);
+
+                    /* this is code that may come in handy later on but im not positive....
                     <div class="listchildren">${listmembers}</div> */
     }
 }
