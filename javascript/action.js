edd6a14dc1c1a9cb3eabae44cdfe9aca8c92a6d8
Nathan Leavitt <nateleavitt@gmail.com>
2019-10-24 17:20:44 -0700

    updated to follow the way ben was doing it.

--- javascript/action.js
+++ javascript/action.js
@@ -1,42 +1,52 @@
 function addListParent(listName) {
 
-    $("#newTodoDiv").show();
     myList.add(listName);
     printPage();
 
-    console.log("This is working");
+    // console.log("This is working");
 }
 
-function addListChild(itemName, listNum) {
-    $(".createListNames").val("");
-    myList.collection[listNum].add(itemName, "Name of the List Items");
-    printPage();
+function addListChild(event, itemName, listNum) {
+    switch (event.which) {
+        case 13: //enter key
+        // $(".createListNames").val("");
+        myList.collection[listNum].add(itemName, "Name of the List Items");
+        printPage();
+        break;
+    }
 }
 
-function editList(listNum, itemnumber, name) {
-    let thename = myList.add(thename)
-    // thename = name;
-    printPage();
+function editListItem(listNum, itemnumber, name) {
+    switch (event.which) {
+        case 13: //enter key
+        let thename = name;
+        // myList.add(thename)
+        printPage();
+        break;
+    }
 }
-
 
 function printPage() {
     console.log("printPage function");
 
-    $(".todoList").html("");
+    $(".lists").html("");
+    $(".todoItems").html("");
 
     for (let i = 0; i < myList.collection.length; i++) {
         let theli = myList.collection[i];
-        let listmembers = "";
+        let listMembers = "";
         for (let g = 0; g < theli.collection.length; g++) {
             let liName = theli.collection[g];
-            listmembers += `<li class="member" contenteditable="true" onkeydown="editList(${i}, ${g}, this.innerText)" >${liName.name}</li>`
+            listMembers += `<li class="member" contenteditable="true" onkeydown="editListItem(${i}, ${g}, this.innerText)" >${liName.name}</li>`
         }
-        // $(".todoList").append(`<input type="text" class="listparent" onkeyup="addListChild(event, this.value, ${i})">`);
-        $(".lists").append(`<li type="text" class="listparent" onkeyup="addListChild(event, this.value, ${i})">${theli.name}</li>`);
-        $(".todoList").append(listmembers);
-
-                    /* this is code that may come in handy later on but im not positive....
-                    <div class="listchildren">${listmembers}</div> */
+        $(".lists").append(`
+            <div>
+                <span>${theli.name}</span>
+                <input type="text" class="listparent" onkeyup="addListChild(event, this.value, ${i})">
+                <ul>${listMembers}</ul>
+            </div>
+            `)
+                    //Notes for tomorrow... I need the name of the list to stay when i hit the enter key. FurtherMore after the enter key is hit and the parent list and its name has been created, i need the child of the list to appear allowing me to edit as a part of the list.
+                    //  this is code that may come in handy later on but im not positive....
     }
 }
